5ce2f406d4544bcc9e909fa23ea9f5bf9d7581d9
armaank1999 <armaankalyanpur@gmail.com>
2021-04-30 12:47:14 -0700

    starter files

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # cibin-template
 
+[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/UCB-stat-159-s21/homework-8-group-6-1/HEAD)
+
 ## TO DO:
 Update this README.md file with information about your package implementation in a similar way to how it is done in [this example](https://github.com/fperez/mytoy). You should include a Binder badge just like that example. You can use that README.md as a starting point if you wish. Your repo should include all of the infrastructure described in that file so that it meets Python packaging requirements.
---
+++ setup.py
@@ -0,0 +1,28 @@
+# A minimal setup.py file to make a Python project installable.
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fh:
+    requirements = [line.strip() for line in fh]
+
+
+setuptools.setup(
+    name             = "CI Bin",
+    version          = "0.0.1",
+    author           = "Armaan Kalyanpur, Olivia McNary, Andrew Kaplan, Mengdi Gao",
+    author_email     = "armaankalyanpur@berkeley.edu",
+    description      = "A Python library to test the efficacy of treatments, such as the Regeneron antibody cocktail's effect on COVID.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    packages         = setuptools.find_packages(),
+    classifiers       = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires  = '>= 3.7',
+    install_requires = requirements,
+)

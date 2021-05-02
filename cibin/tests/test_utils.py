c82f14bc7bf9f07567f0fa3984c86c6e38e64818
Olivia McNary <oliviamcnary@berkeley.edu>
2021-05-02 15:34:51 -0700

    Add test_nchoosem to test_utils.py

--- cibin/tests/test_utils.py
+++ cibin/tests/test_utils.py
@@ -1,3 +1,23 @@
-"""Test cibin.utils functions."""
+"""
+Test cibin.utils functions.
 
-from cibin.utils import *
+Unless otherwise noted, expected returns are calculated using the equivalent
+functions in R.
+"""
+
+import pytest
+import numpy as np
+from ..utils import nchoosem
+
+
+def test_nchoosem():
+    """Test that nchoosem returns correct list."""
+    n = 5
+    m = 3
+    Z_all = nchoosem(n, m)
+    expected = [[1, 1, 1, 0, 0], [1, 1, 0, 1, 0],
+                [1, 1, 0, 0, 1], [1, 0, 1, 1, 0],
+                [1, 0, 1, 0, 1], [1, 0, 0, 1, 1],
+                [0, 1, 1, 1, 0], [0, 1, 1, 0, 1],
+                [0, 1, 0, 1, 1], [0, 0, 1, 1, 1]]
+    assert Z_all == expected

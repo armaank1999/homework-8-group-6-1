ab94ce65a04d71f67876478ed2933b090c083e45
Olivia McNary <oliviamcnary@berkeley.edu>
2021-05-02 16:34:39 -0700

    Add test_combs to test_utils.py

--- cibin/tests/test_utils.py
+++ cibin/tests/test_utils.py
@@ -7,17 +7,27 @@ functions in R.
 
 import pytest
 import numpy as np
-from ..utils import nchoosem
+from ..utils import *
 
 
 def test_nchoosem():
     """Test that nchoosem returns correct list."""
     n = 5
     m = 3
-    Z_all = nchoosem(n, m)
+    Z = nchoosem(n, m)
     expected = [[1, 1, 1, 0, 0], [1, 1, 0, 1, 0],
                 [1, 1, 0, 0, 1], [1, 0, 1, 1, 0],
                 [1, 0, 1, 0, 1], [1, 0, 0, 1, 1],
                 [0, 1, 1, 1, 0], [0, 1, 1, 0, 1],
                 [0, 1, 0, 1, 1], [0, 0, 1, 1, 1]]
-    assert Z_all == expected
+    assert Z == expected
+
+
+def test_combs():
+    """Test that rows of list have correct number of ones."""
+    n = 5
+    m = 3
+    nperm = 10
+    Z = combs(n, m, nperm)
+    for x in Z:
+        assert sum(x) == m

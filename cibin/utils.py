9b068915178cea48e0d240452f74b2a7a5e3065d
Olivia McNary <oliviamcnary@berkeley.edu>
2021-05-02 11:01:42 -0700

    Add nchoosem to utils.py

--- cibin/utils.py
+++ cibin/utils.py
@@ -9,3 +9,23 @@ outcomes and two treatments.
 from itertools import combinations
 from math import comb, floor
 import numpy as np
+
+
+def nchoosem(n, m):
+    """
+    Exact re-randomization matrix for small n choose m.
+
+    Parameters
+    ----------
+    n: int
+        total number of subjects
+    m: int
+        number of subjects with treatment
+    """
+    c = comb(n, m)
+    trt = combinations(np.arange(1, n+1), m)
+    Z = [[None]*n for i in np.arange(c)]
+    for i in np.arange(c):
+        co = next(trt)
+        Z[i] = [1 if j in co else 0 for j in np.arange(1, n+1)]
+    return Z

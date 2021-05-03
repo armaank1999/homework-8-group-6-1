3ee45e0bf61df60a8c9e35c630ca6b6fb806759e
Olivia McNary <oliviamcnary@berkeley.edu>
2021-05-02 18:04:17 -0700

    Add test_pval_two to test_utils.py

--- cibin/tests/test_utils.py
+++ cibin/tests/test_utils.py
@@ -50,3 +50,22 @@ def test_pval_one_lower():
     pval = pval_one_lower(n, m, N, Z_all, tau_obs)
     expected = 0.23684210526315788
     assert pval == expected
+
+
+def test_pval_two():
+    """Test that pval_two returns correct p-value."""
+    n11 = 6
+    n10 = 4
+    n01 = 4
+    n00 = 6
+    n = n11+n10+n01+n00
+    m = n11+n10
+    N01 = 0
+    N10 = 0
+    N11 = 2
+    N = [N11, N10, N01, n-(N11+N10+N01)]
+    Z_all = nchoosem(n, m)
+    tau_obs = n11/m - n01/(n-m)
+    pval = pval_two(n, m, N, Z_all, tau_obs)
+    expected = 0.47368421052631576
+    assert pval == expected
